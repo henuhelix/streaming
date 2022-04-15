ec9f5022def2e681651612e2b17732cbf351978d
moo-the-cow <info@edumail.at>
2022-04-16 00:46:46 +0200

    fixed entrypoint not having srt transmit

--- streaming/entrypoint.sh
+++ streaming/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/tmp/srt/srt-live-transmit -st:yes "srt://127.0.0.1:5002?mode=listener&lossmaxttl=40&latency=2000" "srt://0.0.0.0:5001?mode=listener" & \
+/tmp/srt/srt-live-transmit -st:yes "srt://127.0.0.1:${SRT_ENDPOINT_PORT}?mode=listener&lossmaxttl=40&latency=2000" "srt://0.0.0.0:5001?mode=listener" & \
 /tmp/srtla/srtla_rec 5000 ${SRT_ENDPOINT_ADDR} ${SRT_ENDPOINT_PORT}
 
 #path/to/srt/srt-live-transmit -st:yes "srt://127.0.0.1:5002?mode=listener&lossmaxttl=40&latency=2000" "srt://0.0.0.0:5001?mode=listener" &
